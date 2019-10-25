88b1ff178112b7716ec5f524b5404e52fc3365a8
sr.xu <sr.xu@ctrip.com>
2019-10-25 14:46:41 +0800

    createReactive test

--- test/createReactive.test.ts
+++ test/createReactive.test.ts
@@ -1,25 +1,48 @@
 import 'jest'
-import createReactive from '../src/createReactive'
+import createReactive, { isReactive } from '../src/createReactive'
 
 describe('createReactive', () => {
-    it('should throw error when createReactive got invalide arguments', () => {
+    test('should throw error when createReactive got invalide arguments', () => {
         expect(() => {
             createReactive(undefined)
         }).toThrow()
-
-        expect(() => {
-            createReactive(1 as unknown as object)
-        }).toThrow()
-
-        expect(() => {
-            createReactive('123' as unknown as object)
-        }).toThrow()
-
+        
         expect(() => {
             createReactive(null)
         }).toThrow()
 
-        createReactive([])
-        createReactive({})
+        expect(() => {
+            createReactive(1 as any)
+        }).toThrow()
+
+        expect(() => {
+            createReactive('123' as any)
+        }).toThrow()
+
+        expect(() => {
+            createReactive(true as any)
+        })
+
+        expect(() => {
+            createReactive([])
+        }).not.toThrow()
+
+        expect(() => {
+            createReactive({})
+        }).not.toThrow()
+    })
+})
+
+describe('isReactive', () => {
+    test('can be detected by isReactive', () => {
+        expect(isReactive(undefined)).toBeFalsy()
+        expect(isReactive(null)).toBeFalsy()
+        expect(isReactive(true)).toBeFalsy()
+        expect(isReactive(false)).toBeFalsy()
+        expect(isReactive(1)).toBeFalsy()
+        expect(isReactive('123')).toBeFalsy()
+        expect(isReactive([])).toBeFalsy()
+        expect(isReactive({})).toBeFalsy()
+        expect(isReactive(createReactive({ a: 1 }))).toBeTruthy()
     })
 })
